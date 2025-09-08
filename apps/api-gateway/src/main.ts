73525179c7a7bf383236cc997be7f056c2bcd961
Alloh Nkefor Rodrick <allohnkeforrodrick@gmail.com>
2025-09-08 05:44:11 +0100

    starting with the backend routes

---
+++ apps/api-gateway/src/main.ts
@@ -0,0 +1,50 @@
+import cors from 'cors';
+import proxy from 'express-http-proxy';
+import morgan from 'morgan';
+import rateLimit, { ipKeyGenerator } from 'express-rate-limit';
+import swaggerUi from 'swagger-ui-express';
+import express from 'express';
+import axios from 'axios';
+import cookieParser from 'cookie-parser';
+
+
+
+
+const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+)
+
+app.use(morgan("dev"))
+app.use(express.json({limit: "100mb"}))
+app.use(express.urlencoded({limit: "100mb", extended: true}))
+app.use(cookieParser());
+app.set('trust proxy', 1);
+
+//apply rate limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 mins
+  message: {error: "Too many requests, please try again later!"},
+  standardHeaders: true,
+  legacyHeaders: true,
+  keyGenerator: (req) => ipKeyGenerator(req.ip || "unknown")
+})
+
+app.use(limiter);
+
+app.get('/gateway-health', (req, res) => {
+  res.send({ message: 'Welcome to api-gateway!' });
+});
+
+app.use("/", proxy("http://localhost:6001"))
+
+const port = process.env.PORT || 8080;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on('error', console.error);
